6c7b618eb6e302ceb021f445a773f0c651dc391f
unknown <rangwalaburhanuddin56@gmail.com>
2023-06-02 18:41:28 -0300

    Final Log Parser for Required Data Extraction

--- logparser/add_timestamp.py
+++ logparser/add_timestamp.py
@@ -6,45 +6,6 @@ import os
 import io
 import subprocess
 
-# def follow(thefile):
-#     '''generator function that yields new lines in a file
-#     '''
-#     # seek the end of the file
-#     thefile.seek(0, os.SEEK_END)
-
-#     # start infinite loop
-#     while True:
-#         # read last line of file
-#         line = thefile.readline()
-#         # sleep if file hasn't been updated
-#         if not line:
-#             time.sleep(0.1)
-#             continue
-
-#         yield line
-
-# def follow(filepath):
-#     """
-#     This function tails the log file for new logs and yeilds the new log
-#     :param filepath: path of the log file
-#     :return: new data from the log file
-#     """
-
-#     # Create a subprocess to continuously read updates from the log file
-#     powershell_command = f'Get-Content -Wait -Tail 1 "{filepath}"'
-#     tail_process = subprocess.Popen(['powershell.exe', '-Command', powershell_command], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-
-#     # Continuously read and parse the log file updates
-#     while True:
-#         # Read the new data from the pipe
-#         new_log = tail_process.stdout.readline().decode().strip()
-
-#         # If no new data is available, wait for a short period and continue
-#         if not new_log:
-#             time.sleep(0.2)
-#             continue
-#         yield new_log
-
 def add_timestamp(file_path):
     """
     This function adds timestamp to all the logs in the log file
@@ -52,8 +13,7 @@ def add_timestamp(file_path):
     :return: None
 
     """    
-    arr_log_strings = []
-    idx = 0
+    line_number = 0
 
     powershell_command = f'Get-Content -Wait -Tail 1 "{file_path}"'
     input_process = subprocess.Popen(['powershell.exe', '-Command', powershell_command], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
@@ -67,9 +27,7 @@ def add_timestamp(file_path):
     while True:
         line = input_process.stdout.readline().decode().strip()
         line = ansi_escape.sub('', line)
-        arr_log_strings.append(line)
-        # Check if the input process has terminated
-        # if line == '' and input_process.poll() is not None:
+        line_number += 1
             
         # Write the line to the output file
         to_be_written = '['+datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+']'+" "+ line
@@ -78,15 +36,10 @@ def add_timestamp(file_path):
         print('-----------------------------')
         output_file.write(to_be_written + '\n')
         output_file.flush()
-        if len(arr_log_strings) == 1000:
+        if line_number == 1000:
             output_file.close()
-            idx += 1
             output_file = open('./Logoutput/output_log'+datetime.datetime.now().strftime('%Y%m%d%H%M%S')+'.txt', 'w')
-            arr_log_strings = []
-
-    
-
-
+            line_number = 0
 
 if __name__ == '__main__':
     # (For Debug purposes)
---
+++ logparser/gather_data.py
@@ -0,0 +1,234 @@
+import re
+import datetime
+import pandas as pd
+import os
+
+class Parser:
+    """
+    This class parses the log file and extracts the required information
+    """
+    def __init__(self):
+        self.data_dict = {}
+        self.error_pkts = []
+        self.correct_pkts = []
+        self.correct_pkt_received = False
+        self.num_rounds = 0
+        self.data_frame = pd.DataFrame(columns=['Round Number','Total Errors', 'ERR', 'OK!', 'FAIL!', 'BV_COUNT', 'ERR_POSITIONS'])
+        self.dataframe_number = 0
+    
+    def initalize_round(self):
+        """
+        This function initializes the data dictionary for a new round
+        :return: None
+        """
+        self.num_rounds += 1
+        self.data_dict = {'Round Number': self.num_rounds, 'Total Errors': 0, 'ERR': [], 'OK!': 0, 'FAIL!': 0, 'BV_COUNT': [], 'ERR_POSITIONS': []}
+    
+    def reset_dataframe(self):
+        """
+        This function resets the data frame
+        :return: None
+        """
+        
+        self.data_frame = pd.DataFrame(columns=['Round Number','Total Errors', 'ERR', 'OK!', 'FAIL!', 'BV_COUNT', 'ERR_POSITIONS'])
+    
+    def update_dataframe(self):
+        """ 
+        This function updates the data frame with the new round data dictionary
+        :return: None
+        """
+        new_df = pd.DataFrame([self.data_dict])
+        self.data_frame = pd.concat([self.data_frame, new_df], ignore_index=True)
+        self.dataframe_number += 1
+    
+    def read_complete_file(self, file_path):
+        """
+        This function reads the complete file and returns the logs
+        :param file_path: path of the file
+        :return: list of lines in the file
+        """
+        with open(file_path, 'r') as f:
+            logs = f.readlines()
+        return logs
+
+    def write_to_csv_file(self):
+        """
+        This function writes the data frame to a csv file
+        :return: None
+
+        """
+        fl_nm = './CSVFiles/data'+datetime.datetime.now().strftime('%Y%m%d%H%M%S')+str(self.num_rounds)+'.csv'
+        print('---------Writing to file: ', fl_nm,' ----------')
+        print(self.data_frame.head(5))
+        print(self.data_frame.tail(5))
+        self.data_frame.to_csv(fl_nm, index=False)
+
+    def find_error_positions(self):
+        """
+        
+        This function finds the positions of erroneous bit in the error packets
+        :return: dictionary of error positions and their counts
+        
+        """
+        err_bit_pos = {}
+        print('Correct Packet: \n', self.correct_pkts)
+        print("Packets with errors:\n", self.error_pkts)
+
+        try:
+
+            if len(self.error_pkts) > 0 and len(self.correct_pkts) > 0:
+                correct_packet_bytes = bytearray.fromhex(self.correct_pkts[0].replace(" ", ""))
+
+                # Calculate the number of bit errors and track the positions
+                for err_pkt in self.error_pkts:
+                    bit_errors = 0
+                    error_packet_bytes = bytearray.fromhex(err_pkt.replace(" ", ""))
+                    for i, (error_byte, correct_byte) in enumerate(zip(error_packet_bytes, correct_packet_bytes)):
+                        diff_bits = error_byte ^ correct_byte  # XOR to find differing bits
+                        if diff_bits != 0:
+                            bit_errors += bin(diff_bits).count("1")  # Count the number of set bits
+                            # Track the positions of the differing bits
+                            for bit_pos in range(8):
+                                if diff_bits & (1 << bit_pos):
+                                    error_pos = i * 8 + bit_pos
+                                    if error_pos not in err_bit_pos:
+                                        err_bit_pos[error_pos] = 1
+                                    else:
+                                        err_bit_pos[error_pos] += 1
+        except ValueError:
+            pass
+        return err_bit_pos
+
+    def get_error_positions(self):
+        """
+        This function gets the error positions in the error packets
+        :return None
+        """
+        
+        positions = self.find_error_positions()
+        print("Error Positions", positions)
+        positions = list(positions.keys())
+
+        if 'ERR_POSITIONS' not in self.data_dict:
+            self.data_dict['ERR_POSITIONS'] = positions
+        else:
+            if len(positions) > 0:
+                self.data_dict['ERR_POSITIONS'].append(positions)
+
+        #For the case when there is no correct reception at the end of the round
+        #At that time, the error packets should be preserved till a correct reception is received or error correction is performed
+        if len(self.correct_pkts) > 0:
+            self.error_pkts = []
+
+
+    def parsing_logic(self, log_strings):
+        """
+        This function parses the log strings, updates the data dictionary
+        with reqruired information such as number of errors, number of correct packets, etc.
+        and then updates the data frame and writes it to a csv file
+        :param log_strings: list of log strings
+        :return: None
+        """
+        for log_string in log_strings:
+            log_string = log_string.strip()
+
+            # Removing the ANSI escape characters
+            ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+            log_string = ansi_escape.sub('', log_string)
+            
+            # Regular expression pattern to extract information from the log string
+            pattern = r'\[(.*?)\] \[(.*?)\] (.*)'
+            
+            # Extracting information using regex pattern
+            match = re.match(pattern, log_string)
+            if match:
+                timestamp = match.group(1) #Extracting timestamp
+                packet_type = match.group(2).strip().split(':')[0].strip()  # Extracting the log type
+                data = match.group(3).strip()  # Extracting the log message
+
+                # Storing the information in a dictionary
+                if packet_type == 'ERR':
+                    print(packet_type, data)
+                    if packet_type not in self.data_dict:
+                        self.data_dict[packet_type] = [timestamp]
+                        self.data_dict['Total Errors'] = len(self.data_dict['ERR'])
+                        self.error_pkts.append(data)
+                    else:
+                        self.data_dict[packet_type].append(timestamp)
+                        self.data_dict['Total Errors'] = len(self.data_dict['ERR'])
+                        self.error_pkts.append(data)
+                    print("Error packets till now: ", self.error_pkts)
+                
+                elif packet_type == 'OK!':
+                    print(packet_type, data)
+                    if packet_type not in self.data_dict:
+                        self.data_dict[packet_type] = 1
+                    else:
+                        self.data_dict[packet_type] += 1
+
+                elif packet_type == 'FAIL!':
+                    print(packet_type, data)
+                    if packet_type not in self.data_dict:
+                        self.data_dict[packet_type] = 1
+                    else:
+                        self.data_dict[packet_type] += 1
+                        
+                elif packet_type == 'BV_COUNT':
+                    print(packet_type, data)
+                    if packet_type not in self.data_dict:
+                        self.data_dict[packet_type] = [int(data.strip())]
+                    else:
+                        self.data_dict[packet_type].append(int(data.strip()))
+
+                elif packet_type == 'PKT':
+                    print(packet_type, data)
+                    if not self.correct_pkt_received:
+                        self.correct_pkts.append(data)
+                        self.correct_pkt_received = True
+                    print('Correct packets till now: ', self.correct_pkts)
+
+                elif packet_type == 'nid':
+                    print(packet_type, data)
+                    self.get_error_positions()
+                    self.correct_pkts = []
+                    self.error_pkts = []
+                    self.correct_pkt_received = False
+
+                else:
+                    if data == 'ROUND START':
+                        #This part limits the size of the data frame to 1000 rows and writes it to a csv file
+                        #Reset the data frame and start appending data to it again
+                        print('Length of dataframe till now: ', len(self.data_frame))
+                        if len(self.data_frame) == 1000:
+                            self.write_to_csv_file()
+                            self.reset_dataframe()
+                        self.initalize_round()
+                        print('--------------- ', data, ' : ',self.num_rounds,' ---------------')
+                    
+                    elif data == 'ROUND END':
+                        self.get_error_positions()
+                        
+                        print('Writing to dataframe number: ', self.dataframe_number, " for round: ", self.num_rounds)
+                        print("Dataframe:\n",self.data_dict)
+
+                        self.update_dataframe()
+                        print('--------------- ',data,' : ', self.num_rounds, ' ---------------')
+                print('--------------------------------------------------')    
+
+if __name__ =='__main__':
+    log_parser = Parser()
+    directory = './Logoutput/'
+    file_names = os.listdir(directory)
+
+    # Sort the file names based on modification time
+    sorted_file_names = sorted(file_names, key=lambda x: os.path.getmtime(os.path.join(directory, x)))
+
+    # Read the files in the sorted order
+    for filename in sorted_file_names:
+        # Construct the full file path
+        file_path = os.path.join(directory, filename)
+        print('------------Opening file: ', file_path, ' ------------')
+        # Check if the path is a file
+        if os.path.isfile(file_path):
+            log_strings = log_parser.read_complete_file(file_path)
+            log_parser.parsing_logic(log_strings)
