0bff80c25b11b7257ca2bc419865c89761de8101
Sean Farrelly <sean.farrelly@outlook.com>
2025-05-25 09:25:55 +0400

    🔨 Add tasks for building and packaging firmware (untested)
    (cherry picked from commit 2b0021d44669cbdf0193eb7b8068ee6d5e8b9b50)

---
+++ tasks/utils/build.py
@@ -0,0 +1,38 @@
+import subprocess
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def build_project(board: str, target: str):
+    """Build project"""
+    logger.info(f"Building project ({board=})")
+
+    # Compile app
+    t = subprocess.run(
+        [
+            "make", "clean", f"TARGET={target}", "&&", 
+            "make", "-j16", "node", f"TARGET={target}", f"BOARD={board}",
+            "DEPLOYMENT=nulltb", "PERIOD=1000", "CHN=1", "LOGGING=0", "GPIO=0", "LEDS=1", "NTX=6", "NSLOTS=6", "PWR=ZerodBm", "PROTO=OSF_PROTO_STT"
+        ],
+        cwd="examples/osf-flock"
+    )
+
+    if t.returncode != 0:
+        raise Exception(f"Failed to build project for board {board}")
+
+    # Show build output
+    t = subprocess.run(
+        ["tree", "-L", "4"],
+        cwd="examples/osf-flock",
+        capture_output=False
+    )
+
+    # Generate hex file from .elf
+    t = subprocess.run(
+        ["arm-none-eabi-objcopy", "-O", "ihex", f"build/nrf/{board}/node.elf", f"build/nrf/{board}/node.hex"],
+        cwd="examples/osf-flock",
+    )
+
+    if t.returncode != 0:
+        raise Exception(f"Failed to generate hex file for board {board}")
