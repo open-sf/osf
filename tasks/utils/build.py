8049ec47b130e991d1e41e3673350ecb3a5dc480
Sean Farrelly <sean.farrelly@outlook.com>
2025-05-26 11:18:56 +0400

    🔨 Seperate out clean and build make commands
    (cherry picked from commit 853855d3fd655b84b5f2f3b4f595e83083b8cbf0)

--- tasks/utils/build.py
+++ tasks/utils/build.py
@@ -8,10 +8,15 @@ def build_project(board: str, target: str):
     """Build project"""
     logger.info(f"Building project ({board=})")
 
+    # Clean build directory
+    t = subprocess.run(
+        ["make", "clean", f"TARGET={target}"],
+        cwd="examples/osf-flock"
+    )
+
     # Compile app
     t = subprocess.run(
         [
-            "make", "clean", f"TARGET={target}", "&&", 
             "make", "-j16", "node", f"TARGET={target}", f"BOARD={board}",
             "DEPLOYMENT=nulltb", "PERIOD=1000", "CHN=1", "LOGGING=0", "GPIO=0", "LEDS=1", "NTX=6", "NSLOTS=6", "PWR=ZerodBm", "PROTO=OSF_PROTO_STT"
         ],
