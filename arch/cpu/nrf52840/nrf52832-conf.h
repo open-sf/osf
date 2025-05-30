3025288c0c7dde9ee03ad41d4177050addeb8040
Markus Schuss <markus.schuss@tugraz.at>
2025-05-30 22:55:30 +0200

    Provide a basic (at least CSMA capable) radio based on ESB instead of  IEEE (which is unavailable on nRF52832). Can be forced on other nRF52840/33 devices via make option NRF52840_ESB_RADIO=1

--- arch/cpu/nrf52840/nrf52832-conf.h
+++ arch/cpu/nrf52840/nrf52832-conf.h
@@ -2,7 +2,11 @@
 #ifndef NRF52832_CONF_H_
 #define NRF52832_CONF_H_
 /*---------------------------------------------------------------------------*/
-#define NETSTACK_CONF_RADIO        nrf52840_min_driver
+#if NRF52840_WITH_ESB_RADIO
+#define NETSTACK_CONF_RADIO        nrf52840_esb_driver
+#elif NRF52840_WITH_NULL_RADIO
+#define NETSTACK_CONF_RADIO        nullradio_driver
+#endif
 /*---------------------------------------------------------------------------*/
 #ifndef UART0_CONF_BAUD_RATE
 #define UART0_CONF_BAUD_RATE       NRF_UART_BAUDRATE_115200
--- arch/cpu/nrf52840/nrf52840-conf.h
+++ arch/cpu/nrf52840/nrf52840-conf.h
@@ -2,7 +2,13 @@
 #ifndef NRF52840_CONF_H_
 #define NRF52840_CONF_H_
 /*---------------------------------------------------------------------------*/
+#if NRF52840_WITH_ESB_RADIO
+#define NETSTACK_CONF_RADIO        nrf52840_esb_driver
+#elif NRF52840_WITH_IEEE_RADIO
 #define NETSTACK_CONF_RADIO        nrf52840_ieee_driver
+#elif NRF52840_WITH_NULL_RADIO
+#define NETSTACK_CONF_RADIO        nullradio_driver
+#endif
 /*---------------------------------------------------------------------------*/
 #ifndef UART0_CONF_BAUD_RATE
 #define UART0_CONF_BAUD_RATE       NRF_UART_BAUDRATE_115200
